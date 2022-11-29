21d2eefbbe763e2b4219e85c3c0a9c37d612b5d9
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-11-29 07:53:56 -0500

    Actividades cargadas

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -10,6 +10,23 @@
             img(src="@/assets/template/arrastrar.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
+          h2 Principios de criminalística
+        p.mb-4 #[b Objetivo] Identificar los principios de la criminalística como la base para la investigación y reconstrucción de accidentes de tránsito.
+        .tarjeta.actividad.p-3
+          .row.justify-content-around.align-items-center            
+            .col-sm.mb-3.mb-sm-0
+              p.fw-bold.mb-0 Relación de términos
+            .col-auto
+              a.boton.boton--b(:href="obtenerLink('/actividades/a1/story.html')" target="_blank")
+                span Realizar
+                i.fas.fa-puzzle-piece
+    .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
+      .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
+        .tarjeta.h-100.d-flex.align-items-center.p-4
+          figure
+            img(src="@/assets/template/arrastrar.svg", alt="Texto que describa la imagen")
+      .col-12.col-md-8.col-lg-9
+        .titulo-segundo
           h2 Hipótesis de accidentes de tránsito
         p.mb-4 #[b Objetivo] Identificar hipótesis de accidentes de tránsito y sus factores asociados, como parte fundamental del proceso de investigación y reconstrucción de accidentes de tránsito.
         .tarjeta.actividad.p-3
@@ -17,7 +34,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Relación de términos
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/a2/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
